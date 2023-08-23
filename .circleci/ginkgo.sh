d837eebf4b513ece5346a83a46f187e01d917cb8
fredslbn <fh.silaban@gmail.com>
2023-08-23 12:06:04 +0700

    switch to clang 14

--- .circleci/ginkgo.sh
+++ .circleci/ginkgo.sh
@@ -48,7 +48,7 @@ FINAL_ZIP_ALIAS=Karenulgarde-${TANGGAL}.zip
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=clang17
+COMPILER=clang14
 
 ##----------------------------------------------------------##
 # Specify Linker
